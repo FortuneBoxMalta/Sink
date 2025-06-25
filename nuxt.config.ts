45f6dab175696089dbc7990a6a8fae0fe43fdc41
majcoo1fb <marian.mokosak@fortunebox.com>
2025-06-25 17:26:53 +0200

    dv

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { currentLocales } from './i18n/i18n'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   modules: [
     '@nuxthub/core',
     'shadcn-nuxt',
@@ -12,6 +13,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxtjs/i18n',
   ],
+  ssr: true,
 
   devtools: { enabled: true },
 
