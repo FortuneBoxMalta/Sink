d303eb0d5ab0e881efc137b5d9f3406385255a14
majcoo1fb <marian.mokosak@fortunebox.com>
2025-06-25 19:02:05 +0200

    v

--- server/api/check-httpswrd.ts
+++ server/api/check-httpswrd.ts
@@ -5,28 +5,23 @@ export default defineEventHandler(async (event) => {
   const body = await readBody<{ username: string, password: string }>(event)
   const config = useRuntimeConfig()
 
-  console.log('🔍 BODY:', body)
-  console.log('🔐 CONFIG:', {
-    HTTPSWRD_USERNAME: config.HTTPSWRD_USERNAME,
-    HTTPSWRD_PASSWORD: config.HTTPSWRD_PASSWORD,
-  })
-
   const expectedUsername = config.HTTPSWRD_USERNAME
   const expectedPassword = config.HTTPSWRD_PASSWORD
 
-  if (
-    body.username !== expectedUsername
-    || body.password !== expectedPassword
-  ) {
-    console.log('❌ Unauthorized login attempt')
-    return {
-      success: false,
-      error: 'Unauthorized',
-    }
-  }
+  const authorized = (
+    body.username === expectedUsername
+    && body.password === expectedPassword
+  )
 
-  console.log('✅ Authorized login')
   return {
-    success: true,
+    debug: {
+      received: body,
+      expected: {
+        username: expectedUsername,
+        password: expectedPassword,
+      },
+    },
+    success: authorized,
+    error: authorized ? undefined : 'Unauthorized',
   }
 })
