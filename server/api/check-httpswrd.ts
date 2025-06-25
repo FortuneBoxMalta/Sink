df673d31343e60d65d6becf505719fd74d9f4d7d
majcoo1fb <marian.mokosak@fortunebox.com>
2025-06-25 17:09:35 +0200

    test

--- server/api/check-httpswrd.ts
+++ server/api/check-httpswrd.ts
@@ -1,38 +1,6 @@
-import { defineEventHandler, readBody } from 'h3'
-
-export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig()
-  const body = await readBody<{ username: string, password: string }>(event)
-
-  const correctUsername = config.HTTPSWRD_USERNAME
-  const correctPassword = config.HTTPSWRD_PASSWORD
-
-  if (!correctUsername || !correctPassword) {
-    return {
-      success: false,
-      error: 'Server misconfigured',
-      received: body,
-      expected: {
-        username: correctUsername,
-        password: correctPassword,
-      },
-    }
-  }
-
-  if (
-    body.username === correctUsername
-    && body.password === correctPassword
-  ) {
-    return { success: true }
-  }
-
+export default defineEventHandler(() => {
   return {
-    success: false,
-    error: 'Unauthorized',
-    received: body,
-    expected: {
-      username: correctUsername,
-      password: correctPassword,
-    },
+    status: 'ok',
+    time: Date.now(),
   }
 })
